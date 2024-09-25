80a5d7592de9fbb53698046c7d7bb6d26ba7a354
nadzira <nadziraka19@gmail.com>
2024-09-25 12:01:49 +0700

    Menambahkan viewModel di fragment dan kurang layout home

--- app/src/main/java/com/nadzirakarimantika/dicodingevent/ui/upcoming/UpcomingFragment.kt
+++ app/src/main/java/com/nadzirakarimantika/dicodingevent/ui/upcoming/UpcomingFragment.kt
@@ -1,25 +1,16 @@
 package com.nadzirakarimantika.dicodingevent.ui.upcoming
 
-import android.content.ContentValues.TAG
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.nadzirakarimantika.dicodingevent.data.response.EventResponse
-import com.nadzirakarimantika.dicodingevent.data.response.ListEventsItem
-import com.nadzirakarimantika.dicodingevent.data.retrofit.ApiConfig
 import com.nadzirakarimantika.dicodingevent.databinding.FragmentUpcomingBinding
 import com.nadzirakarimantika.dicodingevent.ui.EventAdapter
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
+
 
 class UpcomingFragment : Fragment() {
 
@@ -32,9 +23,6 @@ class UpcomingFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val upcomingViewModel =
-            ViewModelProvider(this).get(UpcomingViewModel::class.java)
-
         _binding = FragmentUpcomingBinding.inflate(inflater, container, false)
         return binding.root
     }
