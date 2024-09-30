196ceb4463466d8579694cd888d28202ad2647c9
nadzira <nadziraka19@gmail.com>
2024-09-30 16:06:40 +0700

    memperbaiki fitur search

--- app/src/main/java/com/nadzirakarimantika/dicodingevent/ui/finished/FinishedFragment.kt
+++ app/src/main/java/com/nadzirakarimantika/dicodingevent/ui/finished/FinishedFragment.kt
@@ -2,6 +2,8 @@ package com.nadzirakarimantika.dicodingevent.ui.finished
 
 import android.content.Intent
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,6 +11,8 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.android.material.textfield.TextInputEditText
+import com.nadzirakarimantika.dicodingevent.R
 import com.nadzirakarimantika.dicodingevent.data.response.ListEventsItem
 import com.nadzirakarimantika.dicodingevent.databinding.FragmentFinishedBinding
 import com.nadzirakarimantika.dicodingevent.ui.DetailActivity
@@ -43,6 +47,8 @@ class FinishedFragment : Fragment() {
         binding.rvEvent.layoutManager = LinearLayoutManager(requireContext())
         binding.rvEvent.adapter = eventAdapter
 
+        setupSearchBar()
+
         // Observe listEvents from ViewModel and update the adapter when data is available
         finishedViewModel.listEvents.observe(viewLifecycleOwner, Observer { listEvents ->
             eventAdapter.updateEvents(listEvents) // Update the adapter with new data
@@ -55,6 +61,29 @@ class FinishedFragment : Fragment() {
 
         // Fetch events from ViewModel
         finishedViewModel.findEvent()
+    }
+
+    private fun setupSearchBar() {
+        val searchEditText: TextInputEditText = binding.searchBar.findViewById(R.id.searchEditText)
+
+        // Listen for text changes and filter the list
+        searchEditText.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+                val query = s.toString()
+                filterEvents(query)
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+        })
+    }
+
+    private fun filterEvents(query: String) {
+        val originalList = finishedViewModel.listEvents.value ?: emptyList()
+        val filteredList = originalList.filter { event ->
+            event.name?.contains(query, ignoreCase = true) == true
+        }
+        eventAdapter.updateEvents(filteredList)
     }
 
     private fun navigateToDetailEvent(event: ListEventsItem) {
