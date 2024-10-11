94917d81139674f2d9e781ef61ac0cc1a4c44e3a
nadzira <nadziraka19@gmail.com>
2024-10-11 19:21:22 +0700

    menerapkan toast dan membuat textView untuk no event

--- app/src/main/java/com/nadzirakarimantika/dicodingevent/ui/finished/FinishedFragment.kt
+++ app/src/main/java/com/nadzirakarimantika/dicodingevent/ui/finished/FinishedFragment.kt
@@ -2,16 +2,12 @@
 
 package com.nadzirakarimantika.dicodingevent.ui.finished
 
-import android.content.Context
 import android.content.Intent
-import android.net.ConnectivityManager
-import android.net.NetworkCapabilities
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
-import androidx.core.content.ContextCompat.getSystemService
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
